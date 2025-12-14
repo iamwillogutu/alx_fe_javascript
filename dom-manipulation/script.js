2afd1cd900332024f71b9269719ad04a7e2af5f7
iamwillogutu <iamwill.ogutu@gmail.com>
2025-12-14 19:24:11 +0300

    Second Commit

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -7,7 +7,7 @@ const quotes = [
 function showRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const quoteDisplay = document.getElementById("quoteDisplay");
-  quoteDisplay.textContent = quotes[randomIndex].text;
+  quoteDisplay.innerHTML = quotes[randomIndex].text;
 }
 
 function createAddQuoteForm() {
